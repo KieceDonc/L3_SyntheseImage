1d8e534197ac13944a8b881af11cdee9572dda1a
KieceDonc <valentinverst.developer@gmail.com>
2021-10-08 09:59:51 +0200

    TP3

---
+++ TP3/include/jpegtex.h
@@ -0,0 +1,16 @@
+#ifndef JPEGTEX_H
+#define JPEGTEX_H
+
+
+class jpegtex
+{
+    public:
+        jpegtex();
+        virtual ~jpegtex();
+
+    protected:
+
+    private:
+};
+
+#endif // JPEGTEX_H
